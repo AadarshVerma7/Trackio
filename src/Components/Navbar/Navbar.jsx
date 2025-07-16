cd146811925ea9a35063e8652130cb29e0f987f9
Aayush0821 <vatsayush67@gmail.com>
2025-07-16 21:09:48 +0530

    FRONTEND : Adding Glow Animation to Start Now Button

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,30 +1,31 @@
 import React from "react";
 import favicon from '../../assets/favIconPNG.png'
+import {Link} from 'react-router-dom'
 function Navbar() {
   return (
     <div className="w-full px-6 py-4 flex justify-between items-center absolute z-2">
-      <div className="text-3xl font-bold text-orange-400 flex gap-3">
+      <div className="text-3xl font-bold text-orange-400 flex gap-3 cursor-pointer">
         <img className="h-10 " src={favicon} alt="T" />
         <h1>Trackio</h1>
       </div>
 
       <div className="hidden md:flex space-x-8 text-stone-200 text-lg font-medium">
-        <a href="#" className="relative group">
+        <Link to='/' className="relative group">
           Home
           <span className="absolute left-0 -bottom-1 w-0 h-0.25 bg-white transition-all duration-300 group-hover:w-full"></span>
-        </a>
-        <a href="#" className="relative group">
+        </Link>
+        <Link to='/about' className="relative group">
           About
           <span className="absolute left-0 -bottom-1 w-0 h-0.25 bg-white transition-all duration-300 group-hover:w-full"></span>
-        </a>
-        <a href="#" className="relative group">
+        </Link>
+        <Link to='/contact' className="relative group">
           Contact
           <span className="absolute left-0 -bottom-1 w-0 h-0.25 bg-white transition-all duration-300 group-hover:w-full"></span>
-        </a>
-        <a href="#" className="relative group">
+        </Link>
+        <Link to='/join' className="relative group">
           Join Us
           <span className="absolute left-0 -bottom-1 w-0 h-0.25 bg-white transition-all duration-300 group-hover:w-full"></span>
-        </a>
+        </Link>
       </div>
 
 
--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import VantaGlobe from '../../Components/VantaGlobe/VantaGlobe'
+import './LandingPage.css'
+import { useState, useEffect } from 'react'
 function LandingPage() {
+
+    const [showGlow, setShowGlow] = useState(false);
+    useEffect(() => {
+        const showTimer = setTimeout(() => setShowGlow(true), 3000);
+        const hideTimer = setTimeout(() => setShowGlow(false), 6000);
+        return () => {
+            clearTimeout(showTimer);
+            clearTimeout(hideTimer);
+        }
+    }, [])
+
     return (
         <div style={{ position: 'relative' }}>
             <VantaGlobe />
@@ -14,16 +27,16 @@ function LandingPage() {
             }}>
                 <h1>
                     <div className='flex items-baseline'>
-                        <div className='font-baloo font-medium text-7xl text-orange-500'>
-                        Learn,
+                        <div className='font-baloo font-medium text-6xl text-orange-500'>
+                            Learn,
                         </div>
                         <div>
-                            <h1 className='font-baloo text-white font-medium text-8xl transition-all duration-300  hover:scale-90'>
-                            Compete
+                            <h1 className='font-baloo text-white font-medium text-8xl transition-all duration-300  '>
+                                Compete
                             </h1>
                         </div>
                         <div className='font-baloo font-medium text-7xl text-orange-500'>
-                        ,Grow
+                            ,Grow
                         </div>
                     </div>
                 </h1>
@@ -39,19 +52,21 @@ function LandingPage() {
                 <h1 className='font-baloo text-white font-medium text-5xl mt-2.5'>
                     <div className='flex gap-2.5 items-baseline'>
                         All
-                        <div className='text-orange-500 text-6xl transition-all duration-300  hover:scale-90'>
+                        <div className='text-orange-500 text-6xl transition-all duration-300  '>
                             in one
                         </div>
                         place
                     </div>
                 </h1>
-                <button className='mt-7.5 text-xl font-semibold px-7 py-3.5 rounded-xl bg-orange-500 text-white hover:bg-orange-600 hover:-translate-y-1 hover:shadow-[0_12px_24px_rgba(255,102,0,0.4)] transition-all duration-300 ease-in-out active:scale-95 backdrop-blur-md'>
-                    Start Now
-                </button>
+                <div className="glow-wrapper relative inline-block rounded-[22px] p-[2px] mt-10">
+                    <button className="relative z-10 w-full text-white text-xl font-semibold px-7 py-3.5 rounded-[20px] bg-orange-500 hover:bg-orange-600 hover:-translate-y-1 hover:shadow-[0_12px_24px_rgba(255,102,0,0.4)] transition-all duration-300 ease-in-out active:scale-95 cursor-pointer">
+                        Start Now
+                    </button>
+                    <span className={`glow-border pointer-events-none absolute inset-0 rounded-[20px] z-0 ${showGlow ? 'opacity-100' : 'opacity-0'} transition-opacity duration-500`}></span>
+                </div>
+
             </div>
         </div>
-        // </div >
-
     )
 }
 
