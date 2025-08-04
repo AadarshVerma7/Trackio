a1e4274ad6fc7afd426d268585e54f9b76f6d514
AadarshVerma7 <wcsaadarsh@gmail.com>
2025-08-04 14:10:21 +0530

    Adding home page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './Components/Navbar/Navbar'
 import LandingPage from './Pages/LandingPage/LandingPage'
 import Login  from './Pages/LoginPage/Login.jsx';
 import Signup from './Pages/SignupPage/Signup.jsx'
+import HomePage from './Pages/HomePage/HomePage.jsx'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Navbar onLoginClick={() => setShowLogin(true)} onSignupClick={() => setShowSignup(true)}/>
       <Routes>
         <Route path='/' element={<LandingPage />}/>
+        <Route path='home' element={<HomePage />}/>
       </Routes>
 
       {showLogin && (
