b4a5e8c4ecab39270bc5e91b64234f7a70056ae7
AadarshVerma7 <wcsaadarsh@gmail.com>
2025-10-27 14:51:58 +0530

    .......

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5000,
-    allowedHosts: ['.replit.dev', 'localhost']
+    allowedHosts: ['.replit.dev', 'localhost','5a6bf347-5c27-4ff8-9515-56913f82c56b-00-28ytqelp9b7jk.sisko.replit.dev']
   }
 })
